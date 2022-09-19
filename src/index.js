dc583befc86baa7d7a7b842d7527b0fda3f4a51f
svetoslavmanolov <svn.manolov@gmail.com>
2022-09-19 08:42:08 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const routes = require('./routes');
+const port = 5000;
+const app = express();
+
+app.use('/static', express.static('public'));
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+app.use(routes);
+
+app.listen(port, () => console.log(`App is listening on port ${port} ....`))
