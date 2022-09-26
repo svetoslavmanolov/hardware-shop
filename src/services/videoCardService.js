86154be44ff1e54e47b608a8d6e8ee67d2974df9
svetoslavmanolov <svn.manolov@gmail.com>
2022-09-26 09:16:46 +0300

    Add incremented ID to created cubes

--- src/services/videoCardService.js
+++ src/services/videoCardService.js
@@ -5,7 +5,7 @@ const path = require('path');
 exports.getOne = (videoCardId) => videoCards[videoCardId];
 
 exports.save = (videoCard) => {
-    videoCards.push(videoCard)
+    videoCards.push({id: videoCards[videoCards.length - 1].id + 1, ...videoCard});  // {id: videoCards.length, ...videoCards} but now we don't know the last element
     let textData = JSON.stringify(videoCards, '', 4);
     fs.writeFile(path.resolve('src', 'db.json'), textData, {encoding: 'utf-8'})
 
